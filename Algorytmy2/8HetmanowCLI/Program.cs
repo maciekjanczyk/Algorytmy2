4263ac18549d694a29419ab52ad19fc29337c1d4
maciekjanczyk <259014@stud.umk.pl>
2016-10-13 20:45:15 +0200

    Problem 8 Hetmanow - pierwsza wersja

---
+++ Algorytmy2/8HetmanowCLI/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Problem8Hetmanow;
+
+namespace _8HetmanowCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jaki rozmiar szachownicy?: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Hetman8 hetman8 = new Hetman8(n);
+            int[] rozw = hetman8.RozwiazProblem();
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == rozw[j])
+                    {
+                        Console.Write("{0,3} ", rozw[j]);
+                    }
+                    else
+                    {
+                        Console.Write("{0,3} ", '-');
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Algorytmy2/Problem8Hetmanow/8Hetmanow.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem8Hetmanow
+{
+    public class Hetman8
+    {
+        private int[] pozycjaKolumna;
+        private bool[] wWierszu;
+        private bool[] naPrzekatnej1;
+        private bool[] naPrzekatnej2;
+        private int n;
+
+        public Hetman8(int n = 8)
+        {
+            this.n = n;
+            pozycjaKolumna = new int[n];
+            wWierszu = new bool[n];
+            naPrzekatnej1 = new bool[2 * n - 1];
+            naPrzekatnej2 = new bool[2 * n - 1];
+        }
+
+        private int[] Probuj(int i, ref bool q)
+        {            
+            int j = -1;
+
+            while ((!q) && (j < n - 1))
+            {
+                j += 1;
+                q = false;
+
+                if (!wWierszu[j] && !naPrzekatnej1[i + j] && !naPrzekatnej2[System.Math.Abs(i - j)])
+                {
+                    pozycjaKolumna[i] = j;
+                    wWierszu[j] = true;
+                    naPrzekatnej1[i + j] = true;
+                    naPrzekatnej2[System.Math.Abs(i - j)] = true;
+
+                    if (i < n)
+                    {
+                        Probuj(i + 1, ref q);
+
+                        if (!q)
+                        {
+                            wWierszu[j] = false;
+                            naPrzekatnej1[i + j] = false;
+                            naPrzekatnej2[System.Math.Abs(i - j)] = false;
+                        }
+                    }
+                    else
+                    {
+                        q = true;
+                    }
+                }
+            }            
+
+            return pozycjaKolumna;
+        }
+
+        public int[] RozwiazProblem()
+        {
+            bool q = false;
+            return Probuj(0, ref q);
+        }
+    }
+}
