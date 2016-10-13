76e192f521c3f653c3b550640a883dc69ddd5745
maciekjanczyk <259014@stud.umk.pl>
2016-10-13 09:32:07 +0200

    Pierwsza wersja skoczka

--- Algorytmy2/SkoczekCLI/Program.cs
+++ Algorytmy2/SkoczekCLI/Program.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Skoczek
+namespace SkoczekCLI
 {
-    public class Class1
+    class Program
     {
+        static void Main(string[] args)
+        {
+        }
     }
 }
