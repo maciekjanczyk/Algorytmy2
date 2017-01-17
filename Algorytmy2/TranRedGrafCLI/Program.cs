a6c54ed30f0b66f5227f7c0d1eb59838405ae48b
maciekjanczyk <259014@stud.umk.pl>
2017-01-17 19:48:57 +0100

    tranzytywna redukcja

---
+++ Algorytmy2/TranRedGrafCLI/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TranRedGrafCLI
+{
+    class Program
+    {
+        static int[,] WczytajGraf()
+        {
+            Console.Write("Z ilu wierzcholkow sklada sie graf?: ");
+            int iloscWierzcholkow = Convert.ToInt32(Console.ReadLine());
+            int[,] graf = new int[iloscWierzcholkow, iloscWierzcholkow];
+
+            Console.WriteLine("Wpisz graf skierowany (0 - brak krawedzi, 1 - jest krawedz). Liczby oddzielaj spacja:");
+            for (int i = 0; i < iloscWierzcholkow; i++)
+            {
+                string[] linia = Console.ReadLine().Split(' ').ToArray();
+
+                for (int j = 0; j < iloscWierzcholkow; j++)
+                {
+                    graf[i, j] = Convert.ToInt32(linia[j]);
+                }
+            }
+
+            return graf;
+        }
+
+        static void PokazGraf(int[,] graf, string podpis)
+        {
+            int iloscWierzcholkow = graf.GetLength(0);
+
+            Console.WriteLine("{0}: ", podpis);
+
+            for (int i = 0; i < iloscWierzcholkow; i++)
+            {
+                for (int j = 0; j < iloscWierzcholkow; j++)
+                {
+                    Console.Write("{0,2} ", graf[i, j]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[,] graf = WczytajGraf();
+            int[,] tdomkniecie1 = TranzytywnaRedukcja.TranzytywnaRedukcja.Hsu(graf);
+            int[,] tdomkniecie2 = TranzytywnaRedukcja.TranzytywnaRedukcja.Hsu2(graf);
+
+            Console.WriteLine();
+            PokazGraf(graf, "Graf wejsciowy");
+            Console.WriteLine();
+            PokazGraf(tdomkniecie1, "Tranzytywna redukcja metoda Hsu");
+            Console.WriteLine();
+            PokazGraf(tdomkniecie2, "Tranzytywna redukcja metoda Hsu2");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Algorytmy2/TranzytywnaRedukcja/TranzytywnaRedukcja.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TranzytywnaRedukcja
+{
+    public class TranzytywnaRedukcja
+    {
+        public static int[,] Hsu(int[,] graf)
+        {
+            int iloscWezlow = graf.GetLength(0);
+            int[,] ret = new int[iloscWezlow, iloscWezlow];
+
+            for (int i = 0; i < iloscWezlow; i++)
+            {
+                for (int j = 0; j < iloscWezlow; j++)
+                {
+                    ret[i, j] = graf[i, j];
+                }
+            }
+
+            for (int k = 0; k < iloscWezlow; k++)
+            {
+                for (int i = 0; i < iloscWezlow; i++)
+                {
+                    if (ret[i, k] > 0)
+                    {
+                        for (int j = 0; j < iloscWezlow; j++)
+                        {
+                            if (ret[k, j] > 0)
+                            {
+                                ret[i, j] = 0;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return ret;
+        }
+
+        private static int[,] MacierzSciezek(int[,] graf)
+        {
+            int[,] ret = (int[,])graf.Clone();
+            int iloscWezlow = ret.GetLength(0);
+
+            for (int i = 0; i < iloscWezlow; i++)
+            {
+                for (int j = 0; j < iloscWezlow; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+
+                    if (ret[j, i] > 0)
+                    {
+                        for (int k = 0; k < iloscWezlow; k++)
+                        {
+                            if (ret[j, k] == 0)
+                            {
+                                ret[j, k] = ret[i, k];
+                            }
+                        }
+                    }
+                }
+            }
+
+            return ret;
+        }
+
+        public static int[,] Hsu2(int[,] graf)
+        {
+            return Hsu(MacierzSciezek(graf));
+        }
+    }
+}
