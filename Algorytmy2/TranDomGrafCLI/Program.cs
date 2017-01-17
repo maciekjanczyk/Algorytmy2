468b8ac1b343be912fd1ef36b08bc500e6ce0d11
maciekjanczyk <259014@stud.umk.pl>
2017-01-17 19:00:14 +0100

    tranzytywne domkniecie

---
+++ Algorytmy2/TranDomGrafCLI/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TranDomGrafCLI
+{
+    class Program
+    {
+        static int[,] WczytajGraf()
+        {
+            Console.Write("Z ilu wierzcholkow sklada sie graf?: ");
+            int iloscWierzcholkow = Convert.ToInt32(Console.ReadLine());
+            int[,] graf = new int[iloscWierzcholkow, iloscWierzcholkow];
+
+            Console.WriteLine("Wpisz graf skierowany (0 - brak krawedzi, 1 - jest krawedz). Liczby oddzielaj spacja. Pozycje (i,i) oznacz jako 1:");
+            for (int i = 0; i < iloscWierzcholkow; i++)
+            {
+                string[] linia = Console.ReadLine().Split(' ').ToArray();
+
+                for (int j = 0; j < iloscWierzcholkow; j++)
+                {
+                    graf[i, j] = Convert.ToInt32(linia[j]);
+                }
+            }
+
+            return graf;
+        }
+
+        static void PokazGraf(int[,] graf, string podpis)
+        {
+            int iloscWierzcholkow = graf.GetLength(0);
+
+            Console.WriteLine("{0}: ", podpis);
+
+            for (int i = 0; i < iloscWierzcholkow; i++)
+            {
+                for (int j = 0; j < iloscWierzcholkow; j++)
+                {
+                    Console.Write("{0,2} ", graf[i, j]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {            
+            int[,] graf = WczytajGraf();            
+            int[,] tdomkniecie = TranzytywneDomkniecie.TranzytywneDomkniecie.FloydWarshall(graf);
+
+            Console.WriteLine();
+            PokazGraf(graf, "Graf wejsciowy");
+            Console.WriteLine();
+            PokazGraf(tdomkniecie, "Tranzytywne domkniecie");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Algorytmy2/TranzytywneDomkniecie/TranzytywneDomkniecie.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TranzytywneDomkniecie
+{
+    public class TranzytywneDomkniecie
+    {
+        public static int[,] FloydWarshall(int[,] graf)
+        {
+            int iloscWezlow = graf.GetLength(0);
+            int[,] ret = new int[iloscWezlow, iloscWezlow];
+            
+            for (int i = 0; i < iloscWezlow; i++)
+            {
+                for (int j = 0; j < iloscWezlow; j++)
+                {
+                    ret[i, j] = graf[i, j];
+                }
+            }
+
+	        for (int k = 0; k < iloscWezlow; k++)
+	        {
+		        for (int i = 0; i < iloscWezlow; i++)
+		        {
+			        for (int j = 0; j < iloscWezlow; j++)
+			        {
+				        ret[i, j] = ret[i, j] > 0 || (ret[i, k] > 0 && ret[k, j] > 0) ? 1 : 0;
+			        }
+                }       
+	        }
+
+            return ret;          
+        }
+    }
+}
