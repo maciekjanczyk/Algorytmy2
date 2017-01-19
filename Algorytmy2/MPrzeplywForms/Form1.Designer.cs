cb7635d0af62beb2c8482a00ed85bd986d8befac
maciekjanczyk <259014@stud.umk.pl>
2017-01-19 15:19:37 +0100

    przeplyw - winforms

--- Algorytmy2/MPrzeplywForms/Form1.Designer.cs
+++ Algorytmy2/MPrzeplywForms/Form1.Designer.cs
@@ -29,24 +29,71 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(726, 362);
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(181, 362);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Rysowanie";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button1_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 375);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(262, 362);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Drogi";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.richTextBox1.Location = new System.Drawing.Point(651, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(293, 373);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(343, 362);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "FordFulkerson";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(813, 397);
+            this.ClientSize = new System.Drawing.Size(956, 397);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Maksymalny przeplyw";
@@ -54,12 +101,17 @@
             this.Click += new System.EventHandler(this.Form1_Click);
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Algorytmy2/MPrzeplywForms/Form1.cs
+++ Algorytmy2/MPrzeplywForms/Form1.cs
@@ -3,28 +3,36 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using MaksymalnyPrzeplyw;
 
 namespace MPrzeplywForms
 {
     public partial class Form1 : Form
-    {
-        public readonly int NodeWidth = 80;
-        public readonly int NodeHeight = 30;
-
+    {        
+        public bool rysowanie = true;
+        public bool drogi = false;
         public int MouseX { get; private set; }
         public int MouseY { get; private set; }
         public Graphics graphics;
         public List<Rectangle> rects;
-        private Rectangle upRec = Rectangle.Empty;
-        private bool up = false;
+        public List<Label> labels;
+        public List<Tuple<int, int, Label>> polaczenia = new List<Tuple<int, int, Label>>();
+        public int ilosc = 0;
+        public Rectangle zaznaczony1 = Rectangle.Empty;
+        public Rectangle zaznaczony2 = Rectangle.Empty;
+        public int[,] przeplywy;
+        public int[,] przepustowosci;
 
         public Form1()
         {
             rects = new List<Rectangle>();
+            labels = new List<Label>();
             InitializeComponent();            
             graphics = CreateGraphics();          
         }
@@ -45,44 +53,150 @@ namespace MPrzeplywForms
             }
 
             return Rectangle.Empty;
-        } 
+        }
 
-        private void Form1_Click(object sender, EventArgs e)
+        private void cyfraClick(object sender, EventArgs e)
         {
-            if (!up)
-            {                
-                upRec = CurrentRec(MouseX, MouseY);
+            Label lab = (Label)sender;
+            Rectangle rec = rects[labels.IndexOf(lab)];
 
-                if (upRec != Rectangle.Empty)
-                {
-                    up = true;
-                }
-            }  
+            if (zaznaczony1 == Rectangle.Empty)
+            {
+                zaznaczony1 = rec;
+            }
             else
             {
-                upRec.X = MouseX;
-                upRec.Y = MouseY;
-                graphics.DrawRectangle(Pens.Black, upRec);
-                up = false;
+                zaznaczony2 = rec;
+                RysujPolaczenie();
             }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void RysujWezel()
         {
-            Rectangle rectangle = new Rectangle(5, 5, 15, 15);
+            Rectangle rectangle = new Rectangle(MouseX, MouseY, 30, 30);
+            var label = new Label();
+            label.Click += cyfraClick;
+            label.Text = ilosc.ToString();
+            label.Font = new Font(label.Font.FontFamily, (float)13.0);
+            ilosc++;
+            label.Location = new Point(MouseX + 3, MouseY + 3);
+            label.Size = new Size(20, 20);
+            Controls.Add(label);
+            labels.Add(label);
             rects.Add(rectangle);
             graphics.DrawRectangle(Pens.Black, rectangle);
         }
 
-        private void button1_MouseMove(object sender, MouseEventArgs e)
+        private void RysujPolaczenie()
         {
+            Pen pen = new Pen(Color.FromArgb(255, 0, 0, 255), 5);
+            pen.EndCap = LineCap.ArrowAnchor;
+            pen.StartCap = LineCap.RoundAnchor;
 
+            graphics.DrawLine(pen, 
+                new Point(zaznaczony1.X + zaznaczony1.Width/2, zaznaczony1.Y), 
+                new Point(zaznaczony2.X, zaznaczony2.Y));
+
+            int przeplyw = Convert.ToInt32(Interaction.InputBox("Wpisz liczbe", "Wpisz przeplyw: "));
+            int przepustowosc = Convert.ToInt32(Interaction.InputBox("Wpisz liczbe", "Wpisz przepustowosc: "));
+            int idx1 = Convert.ToInt32(labels[rects.IndexOf(zaznaczony1)].Text);
+            int idx2 = Convert.ToInt32(labels[rects.IndexOf(zaznaczony2)].Text);
+
+            przeplywy[idx1, idx2] = przeplyw;
+            przepustowosci[idx1, idx2] = przepustowosc;
+
+            Label text = new Label();
+            text.Text = string.Format("{0}/{1}", przeplyw, przepustowosc);
+            text.AutoSize = true;
+            float x = Math.Abs((((float)(zaznaczony1.X + zaznaczony1.Width / 2)) + zaznaczony2.X)/(float)2.0);
+            float y = Math.Abs(((float)zaznaczony1.Y + (float)zaznaczony2.Y)/(float)2.0);
+            text.Location = new Point((int)x, (int)y);
+            Controls.Add(text);
+
+            polaczenia.Add(new Tuple<int, int, Label>(idx1, idx2, text));
+            WypiszMacierze();
+
+            zaznaczony1 = Rectangle.Empty;
+            zaznaczony2 = Rectangle.Empty;
+        }     
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            if (rysowanie)
+            {
+                RysujWezel();
+            }            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = true;
+            rysowanie = true;
+            drogi = false;
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {            
         }
 
         private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
             MouseX = e.X;
             MouseY = e.Y;
+            label1.Text = string.Format("({0},{1})", MouseX, MouseY);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            przeplywy = new int[ilosc, ilosc];
+            przepustowosci = new int[ilosc, ilosc];
+
+            button1.Enabled = true;
+            button2.Enabled = false;
+            rysowanie = false;
+            drogi = true;
+        }
+
+        private void WypiszMacierze()
+        {
+            richTextBox1.Text = "Przeplywy:\n";
+
+            for (int i = 0; i < ilosc; i++)
+            {
+                for (int j = 0; j < ilosc; j++)
+                {
+                    richTextBox1.Text += string.Format("{0,2} ", przeplywy[i, j]);
+                }
+
+                richTextBox1.Text += "\n";
+            }
+
+            richTextBox1.Text += "\nPrzepustowosci:\n";
+
+            for (int i = 0; i < ilosc; i++)
+            {
+                for (int j = 0; j < ilosc; j++)
+                {
+                    richTextBox1.Text += string.Format("{0,2} ", przepustowosci[i, j]);
+                }
+
+                richTextBox1.Text += "\n";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int[,] GRes = ProblemMaksymalnegoPrzeplywu.GrafResidualny(przeplywy, przepustowosci);
+            ProblemMaksymalnegoPrzeplywu.FordFulkerson(przeplywy, przepustowosci, 0, ilosc - 1);
+
+            foreach (var tupla in polaczenia)
+            {
+                int i = tupla.Item1, j = tupla.Item2;
+                tupla.Item3.Text = string.Format("{0}/{1}", przeplywy[i, j], przepustowosci[i, j]);
+            }
+
+            WypiszMacierze();
         }
     }
 }
