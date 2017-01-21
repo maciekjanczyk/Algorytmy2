7e6403556b5afc79c4ba6aaecb9e8281153b1606
maciekjanczyk <259014@stud.umk.pl>
2017-01-21 21:56:38 +0100

    otoczka wypukla cz2

---
+++ Algorytmy2/OtoczkaWypuklaForms/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace OtoczkaWypuklaForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wyczyscButton = new System.Windows.Forms.Button();
+            this.otoczkaButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // wyczyscButton
+            // 
+            this.wyczyscButton.Location = new System.Drawing.Point(13, 406);
+            this.wyczyscButton.Name = "wyczyscButton";
+            this.wyczyscButton.Size = new System.Drawing.Size(75, 23);
+            this.wyczyscButton.TabIndex = 0;
+            this.wyczyscButton.Text = "Wyczysc";
+            this.wyczyscButton.UseVisualStyleBackColor = true;
+            this.wyczyscButton.Click += new System.EventHandler(this.wyczyscButton_Click);
+            // 
+            // otoczkaButton
+            // 
+            this.otoczkaButton.Location = new System.Drawing.Point(95, 405);
+            this.otoczkaButton.Name = "otoczkaButton";
+            this.otoczkaButton.Size = new System.Drawing.Size(75, 23);
+            this.otoczkaButton.TabIndex = 1;
+            this.otoczkaButton.Text = "Otoczka";
+            this.otoczkaButton.UseVisualStyleBackColor = true;
+            this.otoczkaButton.Click += new System.EventHandler(this.otoczkaButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 441);
+            this.Controls.Add(this.otoczkaButton);
+            this.Controls.Add(this.wyczyscButton);
+            this.Name = "Form1";
+            this.Text = "Otoczka wypukla";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button wyczyscButton;
+        private System.Windows.Forms.Button otoczkaButton;
+    }
+}
+
---
+++ Algorytmy2/OtoczkaWypuklaForms/Form1.cs
@@ -0,0 +1,72 @@
+﻿using GeometriaObliczeniowa;
+using OtoczkaWypukla;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OtoczkaWypuklaForms
+{
+    public partial class Form1 : Form
+    {
+        public Graphics graphics;
+        public int MouseX = 0;
+        public int MouseY = 0;
+        public int PointSize = 8;
+        public List<Punkt> punkty = new List<Punkt>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            graphics = CreateGraphics();            
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            int x = MouseX - PointSize / 2;
+            int y = MouseY - PointSize / 2;
+
+            graphics.FillEllipse(Brushes.Red, new Rectangle(x, y, PointSize, PointSize));
+            punkty.Add(new Punkt(x, y));
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            MouseX = e.X;
+            MouseY = e.Y;
+        }
+
+        private void Wyczysc()
+        {
+            graphics.Clear(Color.White);
+            punkty = new List<Punkt>();
+        }
+
+        private void wyczyscButton_Click(object sender, EventArgs e)
+        {
+            Wyczysc();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Wyczysc();
+        }
+
+        private void otoczkaButton_Click(object sender, EventArgs e)
+        {
+            List<Punkt> H = OtoczkaWypukla.OtoczkaWypukla.ConvexHull(punkty);
+
+            for (int i = 1; i < H.Count; i++)
+            {
+                graphics.DrawLine(Pens.Black,
+                    Convert.ToSingle(H[i - 1].X), Convert.ToSingle(H[i - 1].Y),
+                    Convert.ToSingle(H[i].X), Convert.ToSingle(H[i].Y));
+            }
+        }
+    }
+}
